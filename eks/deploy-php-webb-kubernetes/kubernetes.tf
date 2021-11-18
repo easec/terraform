0741f165b9c1e2c397d94baf61b01822f5af059e
easec <mats.johannesson@easec.se>
2021-11-18 09:07:25 +0100

    Update kubernetes.tf

--- eks/deploy-php-webb-kubernetes/kubernetes.tf
+++ eks/deploy-php-webb-kubernetes/kubernetes.tf
@@ -43,3 +43,50 @@ provider "kubernetes" {
     ]
   }
 }
+
+resource "kubernetes_deployment" "php-webb" {
+  metadata {
+    name = "scalable-php-webb-example"
+    labels = {
+      App = "Scalablephp-webbExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "Scalablephp-webbExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Scalablephp-webbExample"
+        }
+      }
+      spec {
+        container {
+          image = "easec/php-webb"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
