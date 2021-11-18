6ea76464227f006587dab29ee57b82f1eb16fe5f
easec <mats.johannesson@easec.se>
2021-11-18 09:17:06 +0100

    Update kubernetes.tf
    Added LoadBalancer.

--- eks/deploy-php-webb-kubernetes/kubernetes.tf
+++ eks/deploy-php-webb-kubernetes/kubernetes.tf
@@ -90,3 +90,23 @@ resource "kubernetes_deployment" "php-webb" {
   }
 }
 
+resource "kubernetes_service" "php-webb-service" {
+  metadata {
+    name = "php-webb"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
+}
