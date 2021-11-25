4ed7f9cb0a37033068f6fc3fe4389da3bd87279c
easec <mats.johannesson@easec.se>
2021-11-25 20:32:40 +0100

    Create remove.sh

---
+++ eks/weave-flux/remove.sh
@@ -0,0 +1,8 @@
+aws iam delete-role-policy --role-name easec-CodePipelineServiceRole --policy-name codepipeline-access 
+aws iam delete-role --role-name eksworkshop-CodePipelineServiceRole
+aws iam delete-role-policy --role-name eksworkshop-CodeBuildServiceRole --policy-name codebuild-access 
+aws iam delete-role --role-name eksworkshop-CodeBuildServiceRole
+
+
+ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+aws s3 rb s3://easec-${ACCOUNT_ID}-codepipeline-artifacts --force
