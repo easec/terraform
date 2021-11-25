79b1329c74d31bc05b9440cad7c2ee192f79cf9d
easec <mats.johannesson@easec.se>
2021-11-25 20:17:31 +0100

    Create setup.sh

---
+++ eks/weave-flux/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd ~
+mkdir environment && cd environment
+
+ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+aws s3 mb s3://easec-${ACCOUNT_ID}-codepipeline-artifacts
+
+wget https://raw.githubusercontent.com/easec/terraform/main/eks/weave-flux/cpAssumeRolePolicyDocument.json
+
+aws iam create-role --role-name easec-CodePipelineServiceRole --assume-role-policy-document file://cpAssumeRolePolicyDocument.json 
+
+wget https://raw.githubusercontent.com/easec/terraform/main/eks/weave-flux/cpPolicyDocument.json
+
+aws iam put-role-policy --role-name eksworkshop-CodePipelineServiceRole --policy-name codepipeline-access --policy-document file://cpPolicyDocument.json
+
+wget https://raw.githubusercontent.com/easec/terraform/main/eks/weave-flux/cbAssumeRolePolicyDocument.json
+
+aws iam create-role --role-name eksworkshop-CodeBuildServiceRole --assume-role-policy-document file://cbAssumeRolePolicyDocument.json 
+
+wget https://raw.githubusercontent.com/easec/terraform/main/eks/weave-flux/cpPolicyDocument.json
+
+aws iam put-role-policy --role-name eksworkshop-CodeBuildServiceRole --policy-name codebuild-access --policy-document file://cbPolicyDocument.json
