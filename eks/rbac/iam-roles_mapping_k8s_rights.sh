45406576f9c31de3f1b9b7f80ea6d8a8c3eea4d3
easec <mats.johannesson@easec.se>
2021-11-27 19:48:04 +0100

    Create iam-roles_mapping_k8s_rights.sh

---
+++ eks/rbac/iam-roles_mapping_k8s_rights.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+eksctl create iamidentitymapping \
+  --cluster eksworkshop-eksctl \
+  --arn arn:aws:iam::${ACCOUNT_ID}:role/k8sDev \
+  --username dev-user
+
+eksctl create iamidentitymapping \
+  --cluster eksworkshop-eksctl \
+  --arn arn:aws:iam::${ACCOUNT_ID}:role/k8sInteg \
+  --username integ-user
+
+eksctl create iamidentitymapping \
+  --cluster eksworkshop-eksctl \
+  --arn arn:aws:iam::${ACCOUNT_ID}:role/k8sAdmin \
+  --username admin \
+  --group system:masters
