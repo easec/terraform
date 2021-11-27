7cdfa5435604ec09a4a05e5dc52c9ae9ba98a7b9
easec <mats.johannesson@easec.se>
2021-11-27 18:18:42 +0100

    Create k8sAdmin.sh

---
+++ eks/rbac/k8sAdmin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ADMIN_GROUP_POLICY=$(echo -n '{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowAssumeOrganizationAccountRole",
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Resource": "arn:aws:iam::'; echo -n "$ACCOUNT_ID"; echo -n ':role/k8sAdmin"
+    }
+  ]
+}')
+echo ADMIN_GROUP_POLICY=$ADMIN_GROUP_POLICY
+
+aws iam put-group-policy \
+--group-name k8sAdmin \
+--policy-name k8sAdmin-policy \
+--policy-document "$ADMIN_GROUP_POLICY"
