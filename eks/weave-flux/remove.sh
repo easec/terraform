afe31c465ebff3fcc90f14d57790e850fe7a3603
easec <mats.johannesson@easec.se>
2021-11-29 17:07:48 +0100

    Update remove.sh

--- eks/weave-flux/remove.sh
+++ eks/weave-flux/remove.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
+helm uninstall helm-operator --namespace flux
+helm uninstall flux --namespace flux
+kubectl delete namespace flux 
+kubectl delete crd helmreleases.helm.fluxcd.io
+
+helm uninstall mywebserver -n nginx
+kubectl delete namespace nginx
+kubectl delete svc eks-example -n eks-example
+kubectl delete deployment eks-example -n eks-example
+kubectl delete namespace eks-example
+
+
+
 aws iam delete-role-policy --role-name easec-CodePipelineServiceRole --policy-name codepipeline-access 
-aws iam delete-role --role-name eksworkshop-CodePipelineServiceRole
-aws iam delete-role-policy --role-name eksworkshop-CodeBuildServiceRole --policy-name codebuild-access 
+aws iam delete-role --role-name easec-CodePipelineServiceRole
+aws iam delete-role-policy --role-name easec-CodeBuildServiceRole --policy-name codebuild-access 
 aws iam delete-role --role-name eksworkshop-CodeBuildServiceRole
 
 
